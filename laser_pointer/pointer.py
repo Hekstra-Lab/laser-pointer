f99e778f34cb6d72f488840e29053d2443e0b890
ianhi <ianhuntisaak@gmail.com>
2020-09-01 23:50:15 -0400

    allow pcolormesh instead of imshow

--- laser_pointer/pointer.py
+++ laser_pointer/pointer.py
@@ -7,15 +7,23 @@ from IPython import embed
 
 
 class pointer:
-    def __init__(self, image, radius=5, color='red', alpha=.5):
+    def __init__(self, image, ax = None, X = None, Y=None, radius=5, color='red', alpha=.5):
         """
         image_path : str | array
             Path to the image to use
         """
-        self.fig, self.ax = plt.subplots()
+        if ax is None:
+            self.fig, self.ax = plt.subplots()
+        else:
+            self.ax = ax
+            self.fig = ax.get_figure()
         if isinstance(image, str):
             image = plt.imread(image)
-        self.im = self.ax.imshow(image)
+        if X is None or Y is None:
+            self.im = self.ax.imshow(image)
+        else:
+            self.im = self.ax.pcolormesh(X,Y, image, shading='nearest')
+        
         zoom_factory(self.ax)
         if hasattr(self.fig.canvas, 'capture_scroll'):
             self.fig.canvas.capture_scroll = True
