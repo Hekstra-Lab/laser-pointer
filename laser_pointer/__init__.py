22531621eb628e77c41cbeacb11efddafef8aecf
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2020-12-14 12:59:24 -0500

    fold pointer into one object + allow loading of pointer

--- laser_pointer/__init__.py
+++ laser_pointer/__init__.py
@@ -1,3 +1,2 @@
-from .pointer import pointer, make_overlay
-from .clicking import click_handler
+from .pointer import pointer, make_overlay, load_pointer
 from .fitting import fit_transform, fit_transform_pointers
--- laser_pointer/pointer.py
+++ laser_pointer/pointer.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
+import matplotlib.patches as patches
 from .clicking import click_handler
 from mpl_interactions import zoom_factory, panhandler
 from IPython import embed
@@ -39,30 +40,100 @@ class pointer:
             self.fig = ax.get_figure()
         if isinstance(image, str):
             image = plt.imread(image)
+        self._image = image
+        self._X = X
+        self._Y = Y
         if X is None or Y is None:
-            self.im = self.ax.imshow(image)
+            self.im = self.ax.imshow(self._image)
         else:
             self.im = self.ax.pcolormesh(X, Y, image, shading="nearest")
 
         zoom_factory(self.ax)
         if hasattr(self.fig.canvas, "capture_scroll"):
             self.fig.canvas.capture_scroll = True
-        self.click_handler = click_handler(
-            self.fig, self.ax, radius=radius, color=color, alpha=alpha
+
+        self.callback_id = self.fig.canvas.mpl_connect(
+            "button_press_event", self._callback
         )
+        self.alpha = alpha
+        self.radius = radius
+        self.color = color
+        self.Xs = []
+        self.Ys = []
+        self._circles = []
+
         self.ph = panhandler(
             self.fig, button=3
         )  # gotta assign to avoid garbage collection
         self.fig.canvas.mpl_connect("close_event", self._handle_close)
         plt.show()
 
+    def _add_point(self, x, y):
+        self.Xs.append(x)
+        self.Ys.append(y)
+        self._circles.append(
+            patches.Circle(
+                (x, y), radius=self.radius, alpha=self.alpha, color=self.color
+            )
+        )
+        self.ax.add_artist(self._circles[-1])
+
+    def add_points(self, Xs, Ys):
+        for x, y in zip(Xs, Ys):
+            self._add_point(x, y)
+        self.fig.canvas.draw_idle()
+
+    def _callback(self, event):
+        if event.button == 1:
+            if (
+                event.xdata is not None
+                and event.ydata is not None
+                and event.inaxes is self.ax
+            ):
+                # if  (self.xlims[0] < event.xdata < self.x_lims) and  (0 < event.ydata < self.y_max):
+                self._add_point(event.xdata, event.ydata)
+                self.fig.canvas.draw_idle()
+
     def _handle_close(self, event):
-        self.click_handler.save()
+        self.save()
 
-    def save(self, filename):
-        self.click_handler.save(filename)
+    def save(self, filename=None):
+        if filename is None:
+            now = datetime.now().isoformat("-", "seconds").replace(":", "-")
+            filename = f"laser-positions-{now}"
+        print(f"saving as: {filename}")
+        np.savez_compressed(
+            filename,
+            image=self._image,
+            X=self._X,
+            Y=self._Y,
+            Xs=np.array(self.Xs),
+            Ys=np.array(self.Ys),
+        )
 
 
-if __name__ == "__main__":
-    #     lp = laser_pointer('example-image.jpg')
-    embed(colors="neutral")
+def load_pointer(filename, ax=None, **kwargs):
+    """
+    Parameters
+    ----------
+    filename : str
+    ax : matplotlib Axis
+        Optional axis for the pointer to use
+    **kwargs :
+        Passed through to the pointer init function
+
+    Returns
+    -------
+    laser_pointer.pointer
+        Loaded with the contents of the file
+    """
+    arr = np.load(filename, allow_pickle=True)
+    X = arr["X"]
+    Y = arr["Y"]
+    if X == np.array(None):
+        X = None
+    if Y == np.array(None):
+        Y = None
+    p = pointer(arr["image"], X=X, Y=Y, ax=ax, **kwargs)
+    p.add_points(arr["Xs"], arr["Ys"])
+    return p
